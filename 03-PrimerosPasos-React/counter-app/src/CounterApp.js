4bc4710472a9d4a30062329b2cac551ee01104aa
JoseDiazG <josethp@hotmail.com>
2022-04-04 09:31:06 -0500

    Update

---
+++ 03-PrimerosPasos-React/counter-app/src/CounterApp.js
@@ -0,0 +1,33 @@
+//snipe para crear todo un archivo
+//rafcp
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+//Se trabaja con Functional Components
+const CounterApp = ( { value = 10 } ) => {
+
+    const [counter, setCounter ] = useState(value); //[]
+
+    //HandleAdd
+    const handleAdd = () => setCounter(counter + 1);
+    const handleSubtract = () => setCounter( counter - 1);
+    const handleReset = () => setCounter(value);
+
+    return(
+        <>
+            <h1>CounterApp</h1>
+            <h2>{ counter }</h2>
+
+            <button onClick={ handleAdd }>+1</button>
+            <button onClick={ handleReset }>Reset</button>
+            <button onClick={ handleSubtract }>-1</button>
+        </>
+    );
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number
+}
+
+//crear una exportacion por defecto racf
+export default CounterApp;
