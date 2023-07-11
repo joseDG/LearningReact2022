1813783ef976916736948865e579dc75274109d1
JoseDG <josethp1@gmail.com>
2023-07-11 13:26:06 -0500

    update seccion
    se creo un compnente de mejor manera

--- 03-PrimerosPasos-React/counter-app/src/components/CounterApp.jsx
+++ 03-PrimerosPasos-React/counter-app/src/components/CounterApp.jsx
@@ -4,11 +4,20 @@ import { useState } from "react"
 const CounterApp = ({value}) => {
 
   const [counter, setCounter] = useState(value);
+
+  const handleAdd = () => setCounter(counter + 1);
+  const handleRest = () => setCounter(counter - 1);
+  const handleReset = () => setCounter(value);
   
   return (
     <>
       <h1>Conter App</h1>
       <h2>{counter}</h2>
+
+      <button onClick={handleAdd}>+1</button>
+      <button onClick={handleRest}>-1</button>
+      <button onClick={handleReset}>Reset</button>
+
     </>
   )
 }
