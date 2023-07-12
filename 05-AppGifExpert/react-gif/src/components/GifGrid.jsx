8a3ea96780296cc94f92b5f4496db1b387b8a12f
JoseDG <josethp1@gmail.com>
2023-07-12 13:12:28 -0500

    ultima de actuializacion

--- 05-AppGifExpert/react-gif/src/components/GifGrid.jsx
+++ 05-AppGifExpert/react-gif/src/components/GifGrid.jsx
@@ -19,11 +19,11 @@ const GifGrid = ({ category }) => {
   return (
     <>
       <h3>{category}</h3>
-      {isLoading && <h2>Cargando...</h2>}
+      {/* {isLoading && <h2>Cargando...</h2>} */}
 
       <div className="card-grid">
         {
-          images.map((image) => (
+          images & images.map((image) => (
             <GifItem 
               key={image.id} 
               {...image} 
--- 05-AppGifExpert/react-gif/src/helpers/getGifs.js
+++ 05-AppGifExpert/react-gif/src/helpers/getGifs.js
@@ -1,15 +1,17 @@
 export const getGifs = async (category) => {
   const url = `https://api.giphy.com/v1/gifs/search?api_key=JaYNzQ5Afu20IA7BNDT3SlHJTsWArZHc&q=${category}&limit=10`;
   const respuesta = await fetch(url);
-  const resultado = await respuesta.json();
+  const {data} = await respuesta.json();
 
   // console.log(resultado.data)
 
-  const gifs = resultado.data.map((img) => ({
-    id: img.id,
-    title: img.title,
-    url: img.images.downsized_medium.url,
-  }));
+  const gifs = data.map((img) => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images.downsized_medium.url,
+    };
+  });
 
   return gifs;
 };
